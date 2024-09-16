9b0a165a5447c320bd4adf3176272232724b1b68
TheWillW <ytthewillw@gmail.com>
2024-09-16 15:17:13 -0400

    Added the coverPage() and uncoverPage() functions, refactored some code.
    coverPage() / uncoverPage() will have usage later. Needs some work to make it more indicative of what I want to see.

--- public/scripts/helpers.js
+++ public/scripts/helpers.js
@@ -7,3 +7,18 @@
 function generateRandInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+/**
+ * Darken the screen and prevent the user from interacting with the elements.
+ */
+function coverPage() {
+    let pageCover = $('<div id="pageCover"></div>');
+    $('body').append(pageCover);
+}
+
+/**
+ * Remove the dark cover from the screen and return control to user.
+ */
+function uncoverPage() {
+    $('#pageCover').remove();
+}
